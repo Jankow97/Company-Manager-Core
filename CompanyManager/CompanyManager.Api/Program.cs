e5f4bf5b1c2fed22163ca60b574370c48f977b08
Jan Kowalczuk <jankow97@gmail.com>
2020-03-22 04:15:02 +0100

    Create project

---
+++ CompanyManager/CompanyManager.Api/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CompanyManager.Api
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
