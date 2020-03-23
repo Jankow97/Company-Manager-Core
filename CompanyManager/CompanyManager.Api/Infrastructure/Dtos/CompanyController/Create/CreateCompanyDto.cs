ee04becde3c354c9f583fc29063ca1ea64a72dae
Jan Kowalczuk <jankow97@gmail.com>
2020-03-23 16:13:59 +0100

    Add dtos in project

---
+++ CompanyManager/CompanyManager.Api/Infrastructure/Dtos/CompanyController/Create/CreateCompanyDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompanyManager.Api.Infrastructure.Dtos.CompanyController.Create
+{
+    public class CreateCompanyDto
+    {
+    }
+}
