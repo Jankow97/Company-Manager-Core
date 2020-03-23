774781681a75cae50a6057efec47f64790f8a27f
Jan Kowalczuk <jankow97@gmail.com>
2020-03-23 19:18:56 +0100

    Add ViewModels

---
+++ CompanyManager/CompanyManager.Api/Infrastructure/ViewModels/CompanyController/Create/CreateCompanyViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompanyManager.Api.Infrastructure.ViewModels.CompanyController.Create
+{
+    public class CreateCompanyViewModel
+    {
+        public long Id { get; set; }
+    }
+}
